f2dd392dcf7ab7e616ebbecc882918842787b13b
Peter Foley <peter@ifoley.id.au>
2014-09-08 23:24:12 +1000

    Add Puppet function to extract datadirs from hash

---
+++ lib/puppet/parser/functions/datadirs.rb
@@ -0,0 +1,35 @@
+class Hash
+  def find_datadirs
+    result = []
+    result << self['datadirs']
+    result << self[':datadirs']
+    result << self['datadirs:']
+    result << self[':datadirs:']
+    self.values.each do |hash_value|
+      values = [hash_value] unless hash_value.is_a? Array
+      values.each do |value|
+        result += value.find_datadirs if value.is_a? Hash
+      end
+    end
+    result.compact
+  end
+end
+
+module Puppet::Parser::Functions
+  newfunction(:datadirs, :type => :rvalue, :doc => <<-EOS
+              Returns an array of unique datadirs found from the passed in hash.
+              EOS
+             ) do |arguments|
+
+               raise(Puppet::ParseError, "datadirs(): Wrong number of arguments " +
+                     "given (#{arguments.size} for 1)") if arguments.size < 1
+
+               backends = arguments[0]
+
+               unless backends.is_a?(Hash)
+                 raise(Puppet::ParseError, 'datadirs(): Requires a hash to work with')
+               end
+
+               backends.find_datadirs.uniq
+  end
+end
