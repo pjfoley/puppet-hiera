9cb7c98ceea3223d12b2a7c27481ddfb5a657272
Peter Foley <peter@ifoley.id.au>
2014-09-12 22:15:40 +1000

    Functions to extract information from hash

--- lib/puppet/parser/functions/hash_value.rb
+++ lib/puppet/parser/functions/hash_value.rb
@@ -16,21 +16,21 @@ class Hash
 end
 
 module Puppet::Parser::Functions
-  newfunction(:datadirs, :type => :rvalue, :doc => <<-EOS
+  newfunction(:hash_value, :type => :rvalue, :doc => <<-EOS
               Returns an array of unique datadirs found from the passed in hash.
               EOS
              ) do |arguments|
-               Puppet::Parser::Functions.autoloader.loadall
 
                raise(Puppet::ParseError, "datadirs(): Wrong number of arguments " +
-                     "given (#{arguments.size} for 1)") if arguments.size < 1
+                     "given (#{arguments.size} for 2)") if arguments.size < 2
 
                backends = arguments[0]
+               key = arguments[1]
 
                unless backends.is_a?(Hash)
                  raise(Puppet::ParseError, 'datadirs(): Requires a hash to work with')
                end
 
-               backends.find_all_values_for('datadir').uniq
+               backends.find_all_values_for(key).uniq
   end
 end
