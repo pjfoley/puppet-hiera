5bcd9aff3ea70f330678d955ba994f8d8ee7a9e6
Peter Foley <peter@ifoley.id.au>
2014-10-04 22:43:18 +1000

    Update error handling and correctly return paths
    Update to make it explict that datadirs only ever expects 1 argument
    
    Update the directory passing to correctly trip off environment variables

--- lib/puppet/parser/functions/datadirs.rb
+++ lib/puppet/parser/functions/datadirs.rb
@@ -7,7 +7,7 @@ module Puppet::Parser::Functions
              ) do |arguments|
 
                raise(Puppet::ParseError, "datadirs(): Wrong number of arguments " +
-                     "given (#{arguments.size} for 1)") if arguments.size < 1
+                     "given (#{arguments.size} for 1)") if arguments.size != 1
 
                backends = arguments[0]
 
@@ -15,6 +15,6 @@ module Puppet::Parser::Functions
                  raise(Puppet::ParseError, 'datadirs(): Requires a hash to work with')
                end
 
-               backends.extract_hashvalues('datadir').uniq.map {|x| x.to_s.split('%{').first}
+               backends.extract_hashvalues('datadir').uniq.map {|x| x.to_s.split(/\/%+{/).first}
   end
 end
