d3e8f4df0bb10121fc51bb3ca3e222a173dd7803
Peter Foley <peter@ifoley.id.au>
2014-10-04 22:46:04 +1000

    Improve arg validation and 2nd arg can now be an array

--- lib/puppet/parser/functions/extract_hashvalues.rb
+++ lib/puppet/parser/functions/extract_hashvalues.rb
@@ -7,13 +7,17 @@ module Puppet::Parser::Functions
              ) do |arguments|
 
                raise(Puppet::ParseError, "extract_hashvalues(): Wrong number of arguments " +
-                     "given (#{arguments.size} for 2)") if arguments.size < 2
+                     "given (#{arguments.size} for 2)") if arguments.size != 2
 
                backends = arguments[0]
                key = arguments[1]
 
                unless backends.is_a?(Hash)
-                 raise(Puppet::ParseError, 'extract_hashvalues(): Requires a hash to work with')
+                 raise(Puppet::ParseError, 'extract_hashvalues(): Requires the first argument to be a hash')
+               end
+
+               unless key.is_a?(String) || (key.is_a?(Array) && arr_all_strings?(key))
+                 raise(Puppet::ParseError, 'extract_hashvalues(): Requires the second argument to be a string or array of strings')
                end
 
                backends.extract_hashvalues(key).uniq
--- lib/puppet/parser/functions/extract_subhash.rb
+++ lib/puppet/parser/functions/extract_subhash.rb
@@ -2,18 +2,22 @@ require File.expand_path('../../../util/hashextensions', __FILE__)
 
 module Puppet::Parser::Functions
   newfunction(:extract_subhash, :type => :rvalue, :doc => <<-EOS
-              Returns a subhash from hash passed in based on a user provided key.
+              Returns a subhash from hash based on a user provided key.
               EOS
              ) do |arguments|
 
                raise(Puppet::ParseError, "extract_subhash(): Wrong number of arguments " +
-                     "given (#{arguments.size} for 2)") if arguments.size < 2
+                     "given (#{arguments.size} for 2)") if arguments.size != 2
 
                backends = arguments[0]
                extract = arguments[1]
 
                unless backends.is_a?(Hash)
-                 raise(Puppet::ParseError, 'extract_subhash(): Requires a hash to work with')
+                 raise(Puppet::ParseError, 'extract_subhash(): Requires the first argument to be a hash')
+               end
+
+               unless extract.is_a?(String) || (extract.is_a?(Array) && arr_all_strings?(extract))
+                 raise(Puppet::ParseError, 'extract_subhash(): Requires the second argument to be a string or array of strings')
                end
 
                backends.extract_subhash(extract)
