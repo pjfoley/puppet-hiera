4fa3aa028b1aa09330e1c3b892eedbe061907107
Peter Foley <peter@ifoley.id.au>
2014-10-12 22:55:34 +1100

    Make the argument checks more explict

--- lib/puppet/parser/functions/keysdir.rb
+++ lib/puppet/parser/functions/keysdir.rb
@@ -1,11 +1,11 @@
 module Puppet::Parser::Functions
   newfunction(:keysdir, :type => :rvalue, :doc => <<-EOS
-              Returns the directory name for a file.  If the path contains a subdirectory keys it also drops that.
+              Returns the directory name for a file.  If the directory path contains a keys subdirectory remove it from the path.
               EOS
              ) do |arguments|
 
                raise(Puppet::ParseError, "keysdir(): Wrong number of arguments " +
-                     "given (#{arguments.size} for 1)") if arguments.size < 1
+                     "given (#{arguments.size} for 1)") if arguments.size != 1
 
                key = arguments[0]
 
